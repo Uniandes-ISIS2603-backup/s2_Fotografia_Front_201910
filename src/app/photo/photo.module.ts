9cdfe479ff70898219b9ead28dc32bff934674b9
Dany Benavides <da.benavides@uniandes.edu.co>
2019-04-08 13:20:18 -0500

    Cascara photo completa y modulo añadido

--- src/app/photo/photo.module.ts
+++ src/app/photo/photo.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PhotoListComponent } from './photo-list/photo-list.component';
+import { PhotoDetailComponent } from './photo-detail/photo-detail.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [PhotoListComponent]
+  declarations: [PhotoListComponent, PhotoDetailComponent]
 })
 export class PhotoModule { }
--- src/app/photo/photo.ts
+++ src/app/photo/photo.ts
@@ -1,2 +1,31 @@
 export class Photo {
+
+    /*
+    * Atributo correspondiente al id de la foto.
+    */
+    id: number;
+
+    /*
+    * Atributo correspondiente a el nombre de la foto.
+    */
+    nombre: string;
+
+    /**
+     * The photo's id
+     */
+    price: number;
+
+    /*
+    * Atributo correspondiente a la fecha de la foto.
+    */
+    date: Date;
+
+    /**
+    * The photo's name
+    */
+    description: string;
+
+    winner: boolean;
+
+    published: boolean;
 }
