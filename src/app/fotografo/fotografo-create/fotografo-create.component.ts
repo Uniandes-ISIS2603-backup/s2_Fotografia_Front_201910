de8dccde895f554837a8bc2538b2eb830d023401
s.acostav <s.acostav@uniandes.edu.co>
2019-04-11 20:54:21 -0500

    Sirve crear fotografo

---
+++ src/app/fotografo/fotografo-create/fotografo-create.component.ts
@@ -0,0 +1,74 @@
+import {Component, OnInit, Output, EventEmitter} from '@angular/core';
+import {DatePipe} from '@angular/common';
+import {ToastrService} from 'ngx-toastr';
+import {FotografoService} from '../fotografo.service';
+import {Fotografo} from '../fotografo';
+
+@Component({
+    selector: 'app-fotografo-create',
+    templateUrl: './fotografo-create.component.html',
+    styleUrls: ['./fotografo-create.component.css'],
+    providers: [DatePipe]
+})
+export class FotografoCreateComponent implements OnInit {
+
+    /**
+    * Constructor for the component
+    * @param dp DatePipe to format the date.
+    * @param calificacionService The calificacion's services provider
+    * @param toastrService The toastr to show messages to the user
+    */
+    constructor(
+        private dp: DatePipe,
+        private fotografoService: FotografoService,
+        private toastrService: ToastrService
+    ) {}
+
+    /**
+    * The new calificacion
+    */
+    fotografo: Fotografo;
+
+    /**
+    * The output which tells the parent component
+    * that the user no longer wants to create an calificacion
+    */
+    @Output() cancel = new EventEmitter();
+
+    /**
+    * The output which tells the parent component
+    * that the user created a new calificacion
+    */
+    @Output() create = new EventEmitter();
+
+    /**
+    * Creates an calificacion
+    */
+    createFotografo(): Fotografo {
+
+        this.fotografoService.createFotografo(this.fotografo)
+            .subscribe((fotografo) => {
+                this.fotografo = fotografo;
+                this.create.emit();
+                this.toastrService.success("The calificacion was created", "calificacion creation");
+
+            });
+        return this.fotografo;
+    }
+
+    /**
+    * Emits the signal to tell the parent component that the
+    * user no longer wants to create an user
+    */
+    cancelCreation(): void {
+        this.cancel.emit();
+    }
+
+    /**
+    * This function will initialize the component
+    */
+    ngOnInit() {
+        this.fotografo = new Fotografo();
+    }
+
+}
---
+++ src/app/fotografo/fotografo-edit/fotografo-edit.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-fotografo-edit',
+  templateUrl: './fotografo-edit.component.html',
+  styleUrls: ['./fotografo-edit.component.css']
+})
+export class FotografoEditComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
