ceeed339ef01c47472e68f56c457ad1c41f24e84
s.acostav <s.acostav@uniandes.edu.co>
2019-05-17 15:05:10 -0500

    borrar commit

--- src/app/calificacion/calificacion.service.ts
+++ src/app/calificacion/calificacion.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 
-import { Calificacion } from './calificacion';
+import { Calificacion } from '../photo/calificacion';
 import { CalificacionDetail } from './calificacion-detail';
 
 
--- src/app/photo/photo.service.spec.ts
+++ src/app/photo/photo.service.spec.ts
@@ -1,12 +1,26 @@
 import { TestBed } from '@angular/core/testing';
 
 import { PhotoService } from './photo.service';
+import {Photo} from './photo';
+import {Calificacion} from './calificacion';
 
 describe('PhotoService', () => {
+  let injector: TestBed;
+  let service: PhotoService;
+    const photos: Photo[]=require('../../assets/photos.json');
+    const calificaciones: Calificacion[] = require('../../assets/calificaciones.json');
   beforeEach(() => TestBed.configureTestingModule({}));
 
   it('should be created', () => {
     const service: PhotoService = TestBed.get(PhotoService);
     expect(service).toBeTruthy();
   });
+
+  it('#createCalificacion should return value from observable',
+    (done: DoneFn) => {
+    service.createCalificacion(photos[0].id,calificaciones[0]).subscribe(value => {
+        expect(value.puntaje).toEqual(calificaciones[0].puntaje);
+        done();
+        });
+    });
 });
